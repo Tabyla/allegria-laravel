de36cccbd88e21b7991e7262dec5ca45c306adfe
Sanechek <sashaperezhogin123@gmail.com>
2024-05-26 18:12:19 +0500

    updated old db tables and created new brands table, added new seeders

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BrandSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $now = Carbon::now();
+        $brands = ['American Vintage', 'Deha', 'George Gina & Lucy', 'Birkenstock'];
+        foreach ($brands as $brand) {
+            DB::table('brands')->insert([
+                'name' => $brand,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+        }
+    }
+}
