1de73eb1672c712c3e90b39dbf685be224bb974b
Sanechek <sashaperezhogin123@gmail.com>
2024-06-06 22:58:07 +0500

    show default block if user haven`t got a favorite product

--- resources/views/frontend/favorites.blade.php
+++ resources/views/frontend/favorites.blade.php
@@ -5,6 +5,16 @@
     <link rel="stylesheet" href="{{ asset('css/wishlist.css') }}?v={{ time() }}">
 @endsection
 @section('content')
+    @if(!$favorites[0])
+    <section class="default-cart">
+        <h1>Избранных товаров нет</h1>
+        <div class="cart-info text">
+            <img src="{{asset('images/cart-default-image.png')}}" alt="cart img">
+            <p>Добавьте что-то, чтобы сэкономить время и сделать шопинг еще более приятным.</p>
+            <button class="submit_btn"><a href="{{route('catalog')}}">Перейти в каталог</a></button>
+        </div>
+    </section>
+    @else
     <section class="wishlist">
         <div class="products-block">
             <div class="product-row">
@@ -32,4 +42,5 @@
             </div>
         </div>
     </section>
+    @endif
 @endsection
