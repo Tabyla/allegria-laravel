6d83d1320c86fed1aae7bf42e5d4cdecfbffe6a9
Sanechek <sashaperezhogin123@gmail.com>
2024-06-20 00:26:48 +0500

    formatting the code

--- app/Http/Controllers/Backend/SiteController.php
+++ app/Http/Controllers/Backend/SiteController.php
@@ -6,8 +6,6 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 
 class SiteController extends Controller
 {
@@ -19,25 +17,5 @@ class SiteController extends Controller
     public function index(): View
     {
         return view('backend.admin/index');
-    }
-
-    public function pages(): View
-    {
-        $pages = array_map(function ($route) {
-            return [
-                'URI' => $route->uri(),
-                'Server' => $route->getDomain() ?: 'Main',
-                'Action' => $route->getActionName(),
-                'Methods' => $route->methods(),
-                'Name' => $route->getName()
-            ];
-        }, Route::getRoutes()->get());
-
-        return view('backend.admin/pages', ['pages' => $pages]);
-    }
-
-    public function settings(): View
-    {
-        return view('backend.admin/settings', ['user' => Auth::user()]);
     }
 }
